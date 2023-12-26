438d9aa4a380a8b7c5e1519a45991220ecdd76ad
Roopam123 <roopamr123@gmail.com>
2023-12-27 00:14:50 +0530

    creating Restfull APIs

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path")
+
+
+// middleware for the read the incoming responce
+app.use(express.urlencoded({extended:true}))
+// set view engine
+app.set("view engine","ejs")
+// set views folder
+app.set("views",path.join(__dirname,"views"))
+// set public folder
+app.set(express.static(path.join(__dirname,"public")))
 
 
 app.listen(port,()=>{
@@ -8,5 +19,5 @@ app.listen(port,()=>{
 })
 
 app.get('/',(req,res)=>{
-    res.send("Home")
+    res.send("Your app is working successfully")
 })
