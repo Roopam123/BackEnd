c45bcaca249216d688e59c6a91a590e9a9867f5a
Roopam123 <roopamr123@gmail.com>
2023-12-26 23:51:39 +0530

    CRUD

---
+++ notes.js
@@ -0,0 +1,11 @@
+// Rest :- Rest is an architectual style that defined the set of rule to used for creating a web services
+// CRUD :- Create,Read,Update,Delete
+// RestFull APIs - It is used for perform a CRUD opretions
+
+
+// CRUD Opretions:- for performing the CRUD Oprations we are using this method
+// 1. GET - retrive responce
+// 2. POST - submit new Data to the server
+// 3. PUT - Updating existing Data (Complately)
+// 4. PATCH - Updating existing Data partially
+// 5 . DELETE - remote data
